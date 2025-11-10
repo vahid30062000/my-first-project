a7cc530ec8dfb78ed1ed3dac58f31360ebef7444
vahid30062000 <vahidshaik4018@gmail.com>
2025-11-10 10:32:51 +0530

    percentage.py

--- percentage.py
+++ percentage.py
@@ -2,16 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data1.csv')
-
-avg_degree = df.groupby('status')['degree_p'].mean()
-
-plt.figure()
-avg_degree.plot(kind='bar')
+percentage_columns = ['ssc_p', 'hsc_p', 'degree_p', 'mba_p']
+avg_percentages = df.groupby('status')[percentage_columns].mean()
+plt.figure(figsize=(10, 6))
+avg_percentages.plot(kind='bar')
+plt.legend(title='Percentage Types')
 plt.xlabel('Placement Status')
-plt.ylabel('Average Degree Percentage')
-plt.title('Average Degree Percentage vs Placement Status')
+plt.ylabel('Average Percentage')
+plt.title('Average Percentages (SSC, HSC, Degree, MBA) vs Placement Status')
 plt.tight_layout()
-
-
 plt.show()
-
